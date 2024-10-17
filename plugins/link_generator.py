82a44acbaef2b0bf4fc3caf816eff87ffa83462b
MOHAN <mohan326856@gmail.com>
2024-10-17 05:54:26 +0530

    dg

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -4,9 +4,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS,SECRET_CODE_LENGTH,BASE_URL
-from helper_func import encode, get_message_id
+from helper_func import encode, get_message_id, get_messages
 import requests
+import asyncio
 import re
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from secrets import token_hex
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
@@ -165,6 +167,8 @@ async def batch(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={long_link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{long_link}", quote=True, reply_markup=reply_markup)
     await second_message.reply_text(caption, quote=True)
+
+
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
--- plugins/useless.py
+++ plugins/useless.py
@@ -4,13 +4,35 @@ from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
-
+import re
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
+
+@Bot.on_message(filters.command('share') & filters.user(ADMINS))
+async def stats(bot: Bot, message: Message):
+
+    while True:
+        try:
+            first_message = await bot.ask(text = "Send the Poster", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            if(first_message.text=="."):
+                break
+            caption = first_message.caption
+            lines = caption.split("\n")
+            name= lines[0]
+            movie = re.sub(r' - \(\d{4}\)', '', name)
+            word_list = movie.split()    
+            for key in word_list:
+                await first_message.reply_text(f"/add  {key}",quote = True)
+                await first_message.reply_text(f"/filter  {key}",quote = True)
+
+            await first_message.reply_text("/broadcast",quote = True)
+            
+        except:
+            return
 
 
 @Bot.on_message(filters.private & filters.incoming)
